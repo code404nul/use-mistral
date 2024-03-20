61ed63c4013c980500046fe724f6a22e3f06be8c
archIBARBU <84344873+code404nul@users.noreply.github.com>
2024-03-20 17:34:52 +0100

    Create mistral.py

---
+++ mistral.py
@@ -0,0 +1,15 @@
+from transformers import AutoTokenizer, MistralForCausalLM
+from os import system
+
+system("pip --upgrade transformer")
+system("pip --upgrade pytorch")
+
+model = MistralForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
